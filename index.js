40cbae8001eddd36f00fa972b4a670068e72ebcb
Travers Pinkerton <jtpinkerton@gmail.com>
2017-12-14 20:49:49 -0600

    added api/users api

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const User = require('./db');
 
 const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5000);
+// All API endpoints under '/api'
+app.get('/api/users', (req, res) => {
+  // get all users
+  User.find((err, users) => {
+    if (err) return console.error(err);
+
+    // return as json
+    res.json(users);
+    console.log(`Sent ${users.length} users`);
+  })
+});
+
+app.post('/api/users', /*upload.single('image'),*/ (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+
+  user.save((err, user) => {
+    if (err) return console.error(err);
+    console.log(`Saved ${user.name} as a new user in our database!`);
+    res.json(req.body);
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on ${port}`);
